52e8d85719a5e7019568ea32a89bc6701e25fecf
leighad <cdversiontwo@gmail.com>
2020-03-20 21:04:58 -0700

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,41 @@
-class Ship
+class Ship 
+    attr_accessor :name, :type, :booty 
+
+    @@all = []
+
+    def initialize(name, type, booty)
+        @name = name
+        @type = type
+        @booty = booty
+        @@all << self 
+    end
+
+    def self.all
+        @@all 
+    end
+
+    def self.clear
+        @@all = [] 
+    end
 end
+
+# class Ship
+#     attr_reader :name, :type, :booty
+
+#     @@ships = []
+
+#     def initialize(args)
+#       @name = args[:name]
+#       @type = args[:type]
+#       @booty = args[:booty]
+#       @@ships << self
+#     end
+
+#     def self.all
+#       @@ships
+#     end
+
+#     def self.clear
+#       @@ships = []
+#     end
+#   end
